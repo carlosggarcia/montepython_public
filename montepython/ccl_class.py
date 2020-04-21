74a315493f0620c6daa3c6a07e6e9908d54c341e
Emilio Bellini <emilio.bellini@gmail.com>
2020-04-21 17:09:34 +0100

    Polished ccl_class

--- montepython/ccl_class.py
+++ montepython/ccl_class.py
@@ -1,9 +1,5 @@
-import re
-import random
 import numpy as np
-import io_mp
 import pyccl as ccl
-
 
 
 class CCL():
@@ -18,41 +14,31 @@ class CCL():
 
         # Set default parameters
         self.pars = {
-            'h'               :  0.67,
-            'Omega_c'         :  0.27,
-            'Omega_b'         :  0.045,
-            'A_s'             :  2.8,
-            # 'ln10_A_s'        :  3.044522,
-            # 'sigma_8'          :  0.840421163375,
-            'n_s'             :  0.96,
-            'w_0'             : -1.0,
-            'w_a'             :  0.0
+            'h':       0.67,
+            'Omega_c': 0.27,
+            'Omega_b': 0.045,
+            'sigma8': 0.840,
+            'n_s':     0.96,
+            'w0': -1.0,
+            'wa':  0.0
         }
 
-
     def get_cosmo_ccl(self):
-        cosmo_ccl = ccl.Cosmology(
-            h        = self.pars['h'],
-            Omega_c  = self.pars['Omega_c'],
-            Omega_b  = self.pars['Omega_b'],
-            # sigma8   = self.pars['sigma_8'],
-            # A_s      = (10.**(-10.))*np.exp(self.pars['ln10_A_s']),
-            A_s      = self.pars['A_s'],
-            n_s      = self.pars['n_s'],
-            w0       = self.pars['w_0'],
-            wa       = self.pars['w_a'],
-            transfer_function = 'boltzmann_class'
-        )
+        param_dict = dict({'transfer_function': 'boltzmann_class'},
+                          **self.pars)
+        try:
+            param_dict.pop('output')
+        except KeyError:
+            pass
+        cosmo_ccl = ccl.Cosmology(**param_dict)
         return cosmo_ccl
 
     def get_sigma8(self):
         return ccl.sigma8(self.cosmo_ccl)
 
-
     def get_Omegam(self):
-        Omm = self.pars['Omega_c']+self.pars['Omega_b']
+        Omm = self.pars['Omega_c'] + self.pars['Omega_b']
         return Omm
-
 
     def get_S8(self):
         S8 = self.get_sigma8()*(self.get_Omegam()/0.3)**(0.5)
@@ -65,24 +51,27 @@ class CCL():
         return
 
     # Set up the dictionary
-    def set(self,*pars_in,**kars):
-        if len(pars_in)==1:
+    def set(self, *pars_in, **kars):
+        if 'A_s' in pars_in[0].keys():
+            self.pars.pop('sigma8')
+        if len(pars_in) == 1:
             self.pars.update(dict(pars_in[0]))
-        elif len(pars_in)!=0:
+        elif len(pars_in) != 0:
             raise RuntimeError("bad call")
         self.pars.update(kars)
         return True
 
-
     def compute(self, level=[]):
         self.cosmo_ccl = self.get_cosmo_ccl()
-        #### Modified growth part
+        # Modified growth part
         if 'growth_param' in self.pars:
             pk = ccl.boltzmann.get_class_pk_lin(self.cosmo_ccl)
-            pknew = ccl.Pk2D(pkfunc=self.pk2D_new(pk), cosmo=self.cosmo_ccl, is_logp=False)
-            ccl.ccllib.cosmology_compute_linear_power(self.cosmo_ccl.cosmo, pknew.psp, 0)
+            pknew = ccl.Pk2D(pkfunc=self.pk2D_new(pk), cosmo=self.cosmo_ccl,
+                             is_logp=False)
+            ccl.ccllib.cosmology_compute_linear_power(self.cosmo_ccl.cosmo,
+                                                      pknew.psp, 0)
 
-        #self.cosmo_ccl.compute_nonlin_power()
+        # self.cosmo_ccl.compute_nonlin_power()
         ccl.sigma8(self.cosmo_ccl)  # David's suggestion
         return
 
@@ -97,27 +86,26 @@ class CCL():
             elif name == 'S_8':
                 value = self.get_S8()
             else:
-                raise RuntimeError("%s was not recognized as a derived parameter" % name)
+                msg = "%s was not recognized as a derived parameter" % name
+                raise RuntimeError(msg)
             derived[name] = value
 
         return derived
 
     def dpk(self, a):
-     result = 0
-     if self.pars['growth_param'] == 'linear':
-         i = 0
-         while True:
-             pname = 'dpk' + str(i)
-             if pname not in self.pars:
-                 break
-             dpki = self.pars[pname]
-             result += dpki / np.math.factorial(i) * (1-a)**i
-             i += 1
-
-     return result
+        result = 0
+        if self.pars['growth_param'] == 'linear':
+            i = 0
+            while True:
+                pname = 'dpk' + str(i)
+                if pname not in self.pars:
+                    break
+                dpki = self.pars[pname]
+                result += dpki / np.math.factorial(i) * (1-a)**i
+                i += 1
+        return result
 
     def pk2D_new(self, pk):
         def pknew(k, a):
             return (1 + self.dpk(a)) ** 2 * pk.eval(k, a, self.cosmo_ccl)
         return pknew
-
